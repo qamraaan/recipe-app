9d26506f3f2907ec79a2c738cf4f658908508ed9
Mohammad Qamran <qamraaan@gmail.com>
2022-06-20 13:39:50 +0530

    Did something!

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const App = () =>{
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
   const [query, setQuery] = useState('chicken');
+  const [loader, setLoader] = useState(false);
 
   useEffect(()=>{
     getRecipes();
@@ -18,7 +19,11 @@ const App = () =>{
 
 
   const getRecipes = async ()=>{
+  setLoader(true);
   const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+  if(response){
+    setLoader(false)
+  }
   const data = await response.json();
   setRecipes(data.hits);
   console.log(data.hits);
@@ -55,15 +60,16 @@ const App = () =>{
       </form>
       {/* <h1 onClick={() => setCounter(counter+1)}>{counter}</h1> */}
       <div className='recipes'>
-      {recipes.length==0 &&(<div className='not-found'>No recipes found</div>)}
-      {recipes.map(recipe =>(
+      {loader && (<div className='loading'>Loading...</div>)}
+      {/* {!loader && recipes.length==0 &&(<div className='not-found'>Oops! No recipes found</div>)} */}
+      {!loader && recipes.length>0 ? (recipes.map(recipe =>(
         <Recipe 
         key = {recipe.recipe.index}
         title = {recipe.recipe.label}
         image = {recipe.recipe.image}
         calories = {recipe.recipe.calories}
         ingredients = {recipe.recipe.ingredients}/>
-      ))}
+      ))): (<div className='not-found'>Oops! No recipes found</div>)}
     </div>
     <div className='footer'>
       <h3>Developed by Mohammad Kamran</h3>
